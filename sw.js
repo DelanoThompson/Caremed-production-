95d72ef440282bf47f8a02a2b50cf143b437d47d
Delano Thompson <delano.thompson@caremed-group.com>
2026-06-02 15:11:25 +0100

    Bump SW cache to v3 and JS cache bust to v3 to fix blank screen on Android

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE  = 'caremed-pro-v2'
+const CACHE  = 'caremed-pro-v3'
 const ASSETS = ['/', '/index.html', '/manifest.json', '/css/app.css', '/main.js']
 
 self.addEventListener('install', e => {
